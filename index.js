5e3cc0a4f7b20e871b0665e170057cfd3f016ff4
parthbansalyt-jpg <parth.bansalyt@gmail.com>
2026-03-12 14:01:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
-  host: 'voidmcnetwork.falixsrv.me',
-  port: 28350,
-  username: 'AFK_Bot'
-})
+function createBot() {
 
-bot.on('spawn', () => {
-  console.log('Bot joined server')
-})
+  const bot = mineflayer.createBot({
+    host: 'voidmcnetwork.falixsrv.me',
+    port: 28350,
+    username: 'AFK_Bot'
+  })
+
+  bot.on('spawn', () => {
+    console.log('Bot joined server')
+  })
+
+  bot.on('end', () => {
+    console.log('Bot disconnected, reconnecting...')
+    setTimeout(createBot, 5000)
+  })
+
+  bot.on('error', err => console.log(err))
+}
+
+createBot()
