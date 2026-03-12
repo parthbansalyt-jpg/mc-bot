52ab14c6b2325d799754bc7f0e4b133ce981334a
parthbansalyt-jpg <parth.bansalyt@gmail.com>
2026-03-12 21:07:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const mineflayer = require('mineflayer')
 
-function createBot() {
+function startBot() {
 
   const bot = mineflayer.createBot({
     host: 'voidmcnetwork.falixsrv.me',
     port: 28350,
-    username: 'ServerBot67'
+    username: 'ServerKeeperBot',
+    auth: 'offline' // remove if your server is premium
   })
 
   bot.on('spawn', () => {
-    console.log('Bot joined server')
+    console.log('Bot joined the server')
+
+    // Anti-AFK jump every 30 seconds
+    setInterval(() => {
+      bot.setControlState('jump', true)
+      setTimeout(() => bot.setControlState('jump', false), 500)
+    }, 30000)
   })
 
   bot.on('end', () => {
-    console.log('Bot disconnected, reconnecting...')
-    setTimeout(createBot, 500000)
+    console.log('Bot disconnected. Reconnecting in 15 seconds...')
+    setTimeout(startBot, 15000)
   })
 
-  bot.on('error', err => console.log(err))
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+  })
 }
 
-createBot()
+startBot()
